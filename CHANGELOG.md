cbe10a58bd3f160daa4cab4a6709fba56247e56b
Jade <Nils.Jadefalke@gmail.com>
2023-12-22 10:04:58 +0100

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for sort-perf
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Criterion.Main ( defaultMain, bgroup, nf, bench, Benchmark )
+import qualified Data.List (sort)
+import System.Random (randomRIO)
+
+import qualified Sorts.New3WM
+import qualified Sorts.New4WM
+import qualified Sorts.New5WM
+import Control.Monad (replicateM)
+
+main :: IO ()
+main = do
+  tData <- mapM benchmark sizes
+  defaultMain [ bgroup "sort" tData]
+
+sizes :: [Int]
+sizes = [10, 100, 10000, 100000, 500000, 1000000, 2500000]
+
+
+benchmark :: Int -> IO Benchmark
+benchmark n = do
+  dataN <- randoms n
+  pure $ bgroup ("Random - " ++ show n)
+    [ bench "original" $ nf Data.List.sort dataN
+    , bench "3 way merge" $ nf Sorts.New3WM.sort dataN
+    , bench "4 way merge" $ nf Sorts.New4WM.sort dataN
+    , bench "5 way merge" $ nf Sorts.New5WM.sort dataN
+    ]
+
+randoms :: Int -> IO [Int]
+randoms n = replicateM n $ randomRIO (0, 100000)
